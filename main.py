cd865783c99de0dff29711f60e702d57dae77d86
Tobias <72868238+Busky-B@users.noreply.github.com>
2022-01-25 19:44:50 +0100

    Added alot of stuff with images, and directionhandling of said images. Only 4 states so far which are cominations of fallin=true/false and grounded=true/false its turning into spaghetti, should probably try to refactor into classes soon

--- main.py
+++ main.py
@@ -27,7 +27,13 @@ ACTIVEPLATFORM = pygame.Rect((0,0,0,0))
 PLAYERONPLATFORM = False
 GAMESPEED = 2
 RANDOMIZEMAP = True
-
+DEBUG = True
+REVERSED = False
+idle_img_left = "assets/MascotBunnyCharacter/Bunny1/01-Idle/__Bunny1_Idle_000.png"
+idle_img_right = "assets/MascotBunnyCharacter/Bunny1/01-Idle/__Bunny1_Idle_000_REVERSE.png"
+jump_img_left = "assets/MascotBunnyCharacter/Bunny1/06-Jump&Fall/__Bunny1_Fall_000.png"
+jump_img_right = "assets/MascotBunnyCharacter/Bunny1/06-Jump&Fall/__Bunny1_Fall_000_REVERSED.png"
+grounded = GROUNDED
 #COLOR
 WHITE = (255, 255, 255)
 BLACK =(0, 0, 0)
@@ -36,8 +42,6 @@ BLUE = (0, 0, 255)
 
 #Boxes structure declaration
 box1 = {'rect': pygame.Rect(10, WINHEIGHT - 200, 50, 60), 'color': WHITE, 'dir': UPRIGHT, 'speed': 4}
-#box2 = {'rect': pygame.Rect(300, 80, 50, 60), 'color': RED, 'dir': UPLEFT, 'speed': 2}
-#box3 = {'rect': pygame.Rect(300, 80, 50, 60), 'color': BLUE, 'dir': DOWNRIGHT, 'speed': 8}
 obst1 = {'rect': pygame.Rect(10, WINHEIGHT - 150, 100, 50), 'color': RED}
 obst2 = {'rect': pygame.Rect(300, WINHEIGHT - 300, 100, 50), 'color': RED}
 obst3 = {'rect': pygame.Rect(25, WINHEIGHT - 450, 100, 50), 'color': RED}
@@ -45,7 +49,13 @@ obst3 = {'rect': pygame.Rect(25, WINHEIGHT - 450, 100, 50), 'color': RED}
 placeholder = [obst1, obst2, obst3]
 obstacles = [obst1, obst2, obst3]
 b = box1
-player = b['rect']
+player = box1['rect']
+test12 = pygame.image.load(idle_img_left)
+# test12.convert()
+test12 = pygame.transform.scale(test12, (50, 60))
+test_rect = test12.get_rect()
+# test_rect.center = (0, 0) 
+player, b['rect'] = test_rect, test_rect
 
 if RANDOMIZEMAP:
     # Randomize map
@@ -56,18 +66,44 @@ if RANDOMIZEMAP:
         count += 1
         platform_height -= 125
 
+def scale_player_sprite(surf):
+    return pygame.transform.scale(surf, (50, 60))
+'''
+    Handle which sprite to use based on playermovement
+    ---
+    in order to not tax the system (god im just talking out of my ass), a global var is used for not spamming the function
+'''
+def player_sprite_direction_handler():
+    global REVERSED
+
+    if GROUNDED: # player IDLE
+        if not REVERSED:
+            new_img  = scale_player_sprite(pygame.image.load(idle_img_left))
+        else:
+            new_img = scale_player_sprite(pygame.image.load(idle_img_right))
+
+    else: # player FALLING/JUMPING
+        if not REVERSED:
+            new_img  = scale_player_sprite(pygame.image.load(jump_img_left))
+        else:
+            new_img = scale_player_sprite(pygame.image.load(jump_img_right))
+
+    return new_img
 
 # GameLoop
 while True:
+    # this needs to be moved into directionhandler func
+    test12 = player_sprite_direction_handler()
     if player.top == 0:
-        print('WINNER')
-        pygame.quit()
-        sys.exit()
-
+        if not DEBUG:
+            print('WINNER')
+            pygame.quit()
+            sys.exit()
     elif player.bottom == WINHEIGHT:
-        print('GAME OVER!')
-        pygame.quit()
-        sys.exit()
+        if not DEBUG:
+            print('GAME OVER!')
+            pygame.quit()
+            sys.exit()
     for obstacle in obstacles:
         if obstacle['rect'].bottom == WINHEIGHT:
             obstacles.pop(obstacles.index(obstacle))
@@ -81,6 +117,17 @@ while True:
     key_state = pygame.key.get_pressed() # get keyboardstate
     b['rect'].left +=  (key_state[pygame.K_RIGHT] * MOVESPEED) - (key_state[pygame.K_LEFT] * MOVESPEED)
     b['rect'].left +=  (key_state[pygame.K_k] * MOVESPEED) - (key_state[pygame.K_j] * MOVESPEED)
+    
+    '''
+        Handle reversed state of sprite when player moves right or left
+    '''
+    if key_state[pygame.K_k] and not REVERSED:
+        REVERSED = True
+        # test12 = change_direction_player_sprite('right')
+    if key_state[pygame.K_j] and REVERSED:
+        REVERSED = False
+        # test12 = change_direction_player_sprite('left')
+
 
     # Should change this to (if not GROUNDED:)
     if JUMPING or FALLING:
@@ -151,10 +198,15 @@ while True:
         b['rect'].left = WINWIDTH - b['rect'].width
 
         
-    pygame.draw.rect(winSur, b['color'], b['rect'])
+    # pygame.draw.rect(winSur, b['color'], b['rect'])
     
     for obst in obstacles: # Draw platforms
         pygame.draw.rect(winSur, obst['color'], obst['rect'])
+    
+    # drawing image on rect
+    winSur.blit(test12, player)
+    # pygame.draw.rect(winSur,BLUE, test_rect)
+
 
     pygame.display.update()
 
