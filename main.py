423c3eee2ae2f00db342e9087dd56ca026573b17
Busky <tobiasgranbom@gmail.com>
2022-01-26 01:23:40 +0100

    Started to work on idle animations, they work for left idle but is very fast, need to slow it down somehow. Also started to add docstrings in order to keep track inside this spaghetti mess

--- main.py
+++ main.py
@@ -29,6 +29,18 @@ GAMESPEED = 2
 RANDOMIZEMAP = True
 DEBUG = True
 REVERSED = False
+idle_img_left_arr = [
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle1.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle2.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle3.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle4.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle5.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle6.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle7.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle8.png",
+    "assets/MascotBunnyCharacter/Bunny1/01-Idle/idle9.png",
+]
+idle_img_counter = 0
 idle_img_left = "assets/MascotBunnyCharacter/Bunny1/01-Idle/__Bunny1_Idle_000.png"
 idle_img_right = "assets/MascotBunnyCharacter/Bunny1/01-Idle/__Bunny1_Idle_000_REVERSE.png"
 jump_img_left = "assets/MascotBunnyCharacter/Bunny1/06-Jump&Fall/__Bunny1_Fall_000.png"
@@ -41,22 +53,34 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 #Boxes structure declaration
-box1 = {'rect': pygame.Rect(10, WINHEIGHT - 200, 50, 60), 'color': WHITE, 'dir': UPRIGHT, 'speed': 4}
+# box1 = {'rect': pygame.Rect(10, WINHEIGHT - 200, 50, 60), 'color': WHITE, 'dir': UPRIGHT, 'speed': 4}
+'''
+    Creation of inital 3 platforms
+    -----
+    When sprites for platforms are implemented this system becomes obsolete,
+        we can then just create a rect and skip the Dict with color key
+'''
 obst1 = {'rect': pygame.Rect(10, WINHEIGHT - 150, 100, 50), 'color': RED}
 obst2 = {'rect': pygame.Rect(300, WINHEIGHT - 300, 100, 50), 'color': RED}
 obst3 = {'rect': pygame.Rect(25, WINHEIGHT - 450, 100, 50), 'color': RED}
-
-placeholder = [obst1, obst2, obst3]
 obstacles = [obst1, obst2, obst3]
-b = box1
-player = box1['rect']
-test12 = pygame.image.load(idle_img_left)
-# test12.convert()
-test12 = pygame.transform.scale(test12, (50, 60))
-test_rect = test12.get_rect()
-# test_rect.center = (0, 0) 
-player, b['rect'] = test_rect, test_rect
 
+'''
+    Creation of player Surface and Rect
+    -----
+    Transforming scale due to images being to large
+    adds a starting pos for player
+'''
+player_surface = pygame.transform.scale(pygame.image.load(idle_img_left), (50, 60))
+player = player_surface.get_rect() # Creat Rect from the imageSurface
+player.top = WINHEIGHT - 200 # put player starting position at first platform
+
+'''
+    Expression for inputing randomized map
+    -----
+    This is meant as a debugging tool and will probably be removed,
+    randomization needs a rework since maps can be unbeatable
+'''
 if RANDOMIZEMAP:
     # Randomize map
     count = 0
@@ -65,7 +89,11 @@ if RANDOMIZEMAP:
         obstacles.append({'rect': pygame.Rect(random.randint(10, WINWIDTH - 50), platform_height, 100, 50), 'color': RED})
         count += 1
         platform_height -= 125
-
+'''
+    Scales the @param surf object to a set size
+    -----
+    Used by player_sprite_direction_handler()
+'''
 def scale_player_sprite(surf):
     return pygame.transform.scale(surf, (50, 60))
 '''
@@ -74,11 +102,15 @@ def scale_player_sprite(surf):
     in order to not tax the system (god im just talking out of my ass), a global var is used for not spamming the function
 '''
 def player_sprite_direction_handler():
+    # TODO: remake this so it doesn't use a global varible
     global REVERSED
+    global idle_img_counter
+    
 
     if GROUNDED: # player IDLE
         if not REVERSED:
-            new_img  = scale_player_sprite(pygame.image.load(idle_img_left))
+            new_img  = scale_player_sprite(pygame.image.load(idle_img_left_arr[idle_img_counter])) # WORKS NOW
+            idle_img_counter +=1
         else:
             new_img = scale_player_sprite(pygame.image.load(idle_img_right))
 
@@ -92,8 +124,15 @@ def player_sprite_direction_handler():
 
 # GameLoop
 while True:
+    # prevent out of range 
+    if idle_img_counter +1 >= 8:
+        idle_img_counter = 0
     # this needs to be moved into directionhandler func
-    test12 = player_sprite_direction_handler()
+    player_surface = player_sprite_direction_handler()
+
+    '''
+        Check win / loose condition
+    '''
     if player.top == 0:
         if not DEBUG:
             print('WINNER')
@@ -104,36 +143,38 @@ while True:
             print('GAME OVER!')
             pygame.quit()
             sys.exit()
+    '''
+        Remove platforms when they reach the bottom
+    '''
     for obstacle in obstacles:
-        if obstacle['rect'].bottom == WINHEIGHT:
+        if obstacle['rect'].bottom == WINHEIGHT: # remove if rect hits bottom
             obstacles.pop(obstacles.index(obstacle))
-        else:
+        else: # Else move rect down
             obstacle['rect'].bottom +=1 
-    player.bottom += 1
-    # for obst in obstacles['rect']:
-    #     obst.bottom += GAMESPEED
-    # Returns all keys, either true or false. Not good for performance, needed for continous movement
-    # print(pygame.key.get_pressed())
+    player.bottom += 1 # Player needs to be moved down aswell to stay fixed on top of platform
+    '''
+        Handle movement input from keyboard
+    '''
     key_state = pygame.key.get_pressed() # get keyboardstate
-    b['rect'].left +=  (key_state[pygame.K_RIGHT] * MOVESPEED) - (key_state[pygame.K_LEFT] * MOVESPEED)
-    b['rect'].left +=  (key_state[pygame.K_k] * MOVESPEED) - (key_state[pygame.K_j] * MOVESPEED)
+    player.left +=  (key_state[pygame.K_RIGHT] * MOVESPEED) - (key_state[pygame.K_LEFT] * MOVESPEED)
+    player.left +=  (key_state[pygame.K_k] * MOVESPEED) - (key_state[pygame.K_j] * MOVESPEED)
     
     '''
         Handle reversed state of sprite when player moves right or left
     '''
     if key_state[pygame.K_k] and not REVERSED:
         REVERSED = True
-        # test12 = change_direction_player_sprite('right')
+        # player_surface = change_direction_player_sprite('right')
     if key_state[pygame.K_j] and REVERSED:
         REVERSED = False
-        # test12 = change_direction_player_sprite('left')
+        # player_surface = change_direction_player_sprite('left')
 
 
     # Should change this to (if not GROUNDED:)
     if JUMPING or FALLING:
         # GRAVITY
         GRAVITY = 10
-        b['rect'].top += GRAVITY * ACCEL
+        player.top += GRAVITY * ACCEL
         ACCEL += 0.1
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -141,7 +182,6 @@ while True:
             if GROUNDED:#for testing with double jumps or True:
                 FALLING = False
                 if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-                    #b['rect'].top -= 100
                     JUMPING = True
                     GROUNDED = False
                     GRAVITY = 10 
@@ -154,10 +194,10 @@ while True:
 
     winSur.fill(BLACK)
     # check if player on ground
-    if b['rect'].bottom > WINHEIGHT:
+    if player.bottom > WINHEIGHT:
         ACCEL = 1
         GRAVITY = 0
-        b['rect'].top = WINHEIGHT - b['rect'].height
+        player.top = WINHEIGHT - player.height
         GROUNDED = True
         FALLING = False
         JUMPING = False
@@ -168,7 +208,6 @@ while True:
         if player.colliderect(obstacle['rect']):
             if player.bottom <= obstacle['rect'].centery:
                 if not GROUNDED:
-                    print('inside ontop')
                     ACTIVEPLATFORM = obstacle['rect'] 
                     GRAVITY = 0
                     player.bottom = obstacle['rect'].top + 1# + for down
@@ -176,13 +215,11 @@ while True:
                     FALLING = False
                     GROUNDED = True 
             
-            elif b['rect'].bottom > obstacle['rect'].centery:
-                print('inside bottom')
+            elif player.bottom > obstacle['rect'].centery:
                 # player touched bottom side, send back to ground
                 GRAVITY = 10
                 ACCEL = 0.1
-    if not player.colliderect(ACTIVEPLATFORM) and b['rect'].bottom != WINHEIGHT and not JUMPING and not FALLING:
-        print('inside out of platform')
+    if not player.colliderect(ACTIVEPLATFORM) and player.bottom != WINHEIGHT and not JUMPING and not FALLING:
         # make player fall
         FALLING = True
         GROUNDED = False
@@ -191,11 +228,11 @@ while True:
 
 
     # The box has moved past the left side.
-    if b['rect'].left < 0:
-        b['rect'].left = 0
+    if player.left < 0:
+        player.left = 0
     # The box has moved past the right side.
-    if b['rect'].right > WINWIDTH:
-        b['rect'].left = WINWIDTH - b['rect'].width
+    if player.right > WINWIDTH:
+        player.left = WINWIDTH - player.width
 
         
     # pygame.draw.rect(winSur, b['color'], b['rect'])
@@ -204,7 +241,7 @@ while True:
         pygame.draw.rect(winSur, obst['color'], obst['rect'])
     
     # drawing image on rect
-    winSur.blit(test12, player)
+    winSur.blit(player_surface, player)
     # pygame.draw.rect(winSur,BLUE, test_rect)
 
 
